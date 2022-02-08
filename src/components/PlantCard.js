9661f4c0d078238fed8d313441859e80d9b7bd67
Adam La Rosa <adam.larosa@flatironschool.com>
2022-02-08 08:54:37 -0800

    solution

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,9 +3,7 @@ import React, { useState } from "react";
 function PlantCard({ plant }) {
 
   const [inStock, setIsInStock] = useState(true)
-  const [showChange, setShowChange] = useState(false)
 
-  const [newPrice, setNewPrice] = useState()
 
   const changeAvailability = () => {
 
@@ -17,27 +15,15 @@ function PlantCard({ plant }) {
     <li className="card">
       <img src={plant.image} alt={"plant name"} />
       <h4>{plant.name}</h4>
-      <p>
-        {showChange ?  
-          <div>
-            <input placeholder={plant.price} type="number" />
-            <button>CHANGE IT!</button>
-          </div> : 
-          `Price: ${plant.price}` 
-        }
-        <button onClick={() => setShowChange(!showChange)}>
-          {showChange ? "go back": "change price" }
-        </button>
-      </p>
-
+      {`Price: ${plant.price}` }
+      
 
       {inStock ? (
         <button onClick={changeAvailability} className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
-
-
+    
 
     </li>
   );
