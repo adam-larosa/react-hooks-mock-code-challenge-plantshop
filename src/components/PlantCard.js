5f4c4d0902497ba3233ab72e8e195b0aa9030cf7
Adam La Rosa <adam.larosa@flatironschool.com>
2022-07-27 08:15:23 -0700

    Revert "solution"
    This reverts commit 9661f4c0d078238fed8d313441859e80d9b7bd67.

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 function PlantCard({ plant }) {
 
   const [inStock, setIsInStock] = useState(true)
+  const [showChange, setShowChange] = useState(false)
 
+  const [newPrice, setNewPrice] = useState()
 
   const changeAvailability = () => {
 
@@ -15,15 +17,27 @@ function PlantCard({ plant }) {
     <li className="card">
       <img src={plant.image} alt={"plant name"} />
       <h4>{plant.name}</h4>
-      {`Price: ${plant.price}` }
-      
+      <p>
+        {showChange ?  
+          <div>
+            <input placeholder={plant.price} type="number" />
+            <button>CHANGE IT!</button>
+          </div> : 
+          `Price: ${plant.price}` 
+        }
+        <button onClick={() => setShowChange(!showChange)}>
+          {showChange ? "go back": "change price" }
+        </button>
+      </p>
+
 
       {inStock ? (
         <button onClick={changeAvailability} className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
-    
+
+
 
     </li>
   );
